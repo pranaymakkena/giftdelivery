1a2706c6d6b1b729f1fb001c9fb2342c4844063b
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2024-04-05 11:51:17 +0530

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Anonymous Gift Delivery!</h1>
+      {/* Add featured gifts or other content here */}
+    </div>
+  );
+};
+
+export default Home;
