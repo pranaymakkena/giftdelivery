b5a489d0b33d2508c788c812ff3b2e82412a83a4
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2024-04-05 11:51:28 +0530

    Create Gifts.js

---
+++ src/pages/Gifts.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GiftList from '../components/GiftList';
+
+const Gifts = () => {
+  return (
+    <div>
+      <h1>Choose a Gift</h1>
+      <GiftList />
+    </div>
+  );
+};
+
+export default Gifts;
