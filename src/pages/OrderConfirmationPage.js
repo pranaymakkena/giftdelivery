ae462c5ee074fff71c59021adaf56d6b75196f31
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2024-04-05 11:52:11 +0530

    Create OrderConfirmationPage.js

---
+++ src/pages/OrderConfirmationPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import OrderConfirmation from '../components/OrderConfirmation';
+
+const OrderConfirmationPage = () => {
+  return (
+    <div>
+      <h1>Order Confirmation</h1>
+      <OrderConfirmation />
+    </div>
+  );
+};
+
+export default OrderConfirmationPage;
