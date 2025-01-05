12e1405baebe7698f904f091ea5dd9cd8b83e6bc
Pranay Makkena <60232491+pranaymakkena@users.noreply.github.com>
2025-01-05 16:40:30 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  // Import Routes instead of Switch
 import GiftCardList from './GiftCardList';
 import GiftCardDetail from './GiftCardDetail';
 import Checkout from './Checkout';
@@ -7,11 +7,11 @@ import Checkout from './Checkout';
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={GiftCardList} />
-        <Route path="/gift-card/:id" component={GiftCardDetail} />
-        <Route path="/checkout" component={Checkout} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<GiftCardList />} />  {/* Use 'element' prop instead of 'component' */}
+        <Route path="/gift-card/:id" element={<GiftCardDetail />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
     </Router>
   );
 };
